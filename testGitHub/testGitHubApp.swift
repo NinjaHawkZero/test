1d331d008321cb3cdf856c11fe94a6fbdd9d9bac
Marcus Westbrooks <marcus.westbrooks@gmail.com>
2023-06-02 13:32:24 -0400

    firstCommit

---
+++ testGitHub/testGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitHubApp.swift
+//  testGitHub
+//
+//  Created by Marcus Westbrooks on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct testGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
